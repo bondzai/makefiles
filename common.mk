6a3b6aa2b7bee7f4a0e42c35f5fa97cebd424dc6
JamesBond <introbond.dev@gmail.com>
2023-12-28 06:21:19 +0700

    Add Docker-related commands and create Prettier configuration file

--- common.mk
+++ common.mk
@@ -1,13 +1,13 @@
+.PHONY: create-prettier-config format up down start stop restart build_broker build_front up_build
+
 FRONT_END_BINARY=frontApp
 BROKER_BINARY=brokerApp
 
-## up: starts all containers in the background without forcing build
 up:
 	@echo "Starting Docker images..."
 	docker-compose up -d
 	@echo "Docker images started!"
 
-## up_build: stops docker-compose (if running), builds all projects and starts docker compose
 up_build: build_broker
 	@echo "Stopping docker images (if running...)"
 	docker-compose down
@@ -15,34 +15,47 @@ up_build: build_broker
 	docker-compose up --build -d
 	@echo "Docker images built and started!"
 
-## down: stop docker compose
 down:
 	@echo "Stopping docker compose..."
 	docker-compose down
 	@echo "Done!"
 
-## build_broker: builds the broker binary as a linux executable
 build_broker:
 	@echo "Building broker binary..."
 	cd ../broker-service && env GOOS=linux CGO_ENABLED=0 go build -o ${BROKER_BINARY} ./cmd/api
 	@echo "Done!"
 
-## build_front: builds the frone end binary
 build_front:
 	@echo "Building front end binary..."
 	cd ../frontend && env CGO_ENABLED=0 go build -o ${FRONT_END_BINARY} ./cmd/web
 	@echo "Done!"
 
-## start: starts the front end
 start: build_front
 	@echo "Starting front end"
 	cd ../frontend && ./${FRONT_END_BINARY} &
 
-## stop: stop the front end
 stop:
 	@echo "Stopping front end..."
 	@-pkill -SIGTERM -f "./${FRONT_END_BINARY}"
 	@echo "Stopped front end!"
 
-## restart: restart the front end
 restart: stop start
+
+create-prettier-config:
+	@echo 'Creating .prettierrc.json...'
+	@if [ ! -e .prettierrc.json ]; then \
+		echo 'Creating default .prettierrc.json configuration file...'; \
+		echo '{' > .prettierrc.json; \
+		echo '    "semi": false,' >> .prettierrc.json; \
+		echo '    "singleQuote": true,' >> .prettierrc.json; \
+		echo '    "tabWidth": 4,' >> .prettierrc.json; \
+		echo '    "printWidth": 80,' >> .prettierrc.json; \
+		echo '    "trailingComma": "all"' >> .prettierrc.json; \
+		echo '}' >> .prettierrc.json; \
+		echo 'Done! .prettierrc.json file created with default settings.'; \
+	fi
+
+format:
+	@echo 'Formatting code...'
+	@prettier --write "**/*.js"
+	@echo 'Done!'
